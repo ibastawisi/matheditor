b670c605b8b06aa251f2d3a441cebe07f5a8a4d1
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-05-07 19:44:52 +0200

    fix color scheme toggle

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,13 +1,24 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo, useEffect } from "react";
 
 export const ColorModeContext = createContext({ toggleColorMode: () => { } });
 
 export default function ToggleColorMode({ children }: { children: React.ReactNode }) {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const [mode, setMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
-  const colorMode = useMemo(() => ({ toggleColorMode: () => { setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light')); }, }), [],);
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () => {
+      setMode((prevMode) => prevMode === 'light' ? 'dark' : 'light');
+    },
+  }), []);
+
+  useEffect(() => {
+    document.body.setAttribute('theme', mode);
+    const colorScheme = document.querySelector('meta[name="color-scheme"]') as HTMLMetaElement;
+    colorScheme.content = mode;
+  }, [mode]);
+
   const theme = useMemo(() => createTheme({ palette: { mode, }, }), [mode],);
 
   return (
