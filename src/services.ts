fd83cf0124ccfb808771bb9456a7113077eb7dec
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-07-26 03:22:58 +0200

    update api url to sub-domain

--- src/services.ts
+++ src/services.ts
@@ -3,27 +3,27 @@ import { EditorDocument } from './slices/app';
 import { BACKEND_URL } from './config';
 
 const createDocument = async (data: EditorDocument) => {
-  const response = await axios.post(BACKEND_URL + '/api/documents', data, { withCredentials: true })
+  const response = await axios.post(BACKEND_URL + '/documents', data, { withCredentials: true })
   return response.data;
 }
 
 const updateDocument = async (data: EditorDocument) => {
-  const response = await axios.put(BACKEND_URL + `/api/documents/${data.id}`, data, { withCredentials: true })
+  const response = await axios.put(BACKEND_URL + `/documents/${data.id}`, data, { withCredentials: true })
   return response.data;
 }
 
 const deleteDocument = async (id: string) => {
-  const response = await axios.delete(BACKEND_URL + `/api/documents/${id}`, { withCredentials: true })
+  const response = await axios.delete(BACKEND_URL + `/documents/${id}`, { withCredentials: true })
   return response.data;
 }
 
 const getDocument = async (id: string) => {
-  const response = await axios.get(`${BACKEND_URL}/api/documents/${id}`, { withCredentials: true })
+  const response = await axios.get(`${BACKEND_URL}/documents/${id}`, { withCredentials: true })
   return response.data;
 }
 
 const getAuthenticatedUser = async () => {
-  const response = await axios.get(BACKEND_URL + '/api/users/me', { withCredentials: true })
+  const response = await axios.get(BACKEND_URL + '/users/me', { withCredentials: true })
   return response.data;
 }
 
