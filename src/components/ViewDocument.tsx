ac1a4797af40a3fe29cbc81e9947f8d91714ba66
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-07-28 04:51:09 +0200

    add fork button sliding animation

--- src/components/ViewDocument.tsx
+++ src/components/ViewDocument.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../slices";
 import { AppDispatch } from "../store";
@@ -11,12 +11,20 @@ import { Helmet } from "react-helmet";
 import { EditorDocument } from "../slices/app";
 import Fab from "@mui/material/Fab";
 import EditIcon from '@mui/icons-material/Edit';
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
+import Fade from "@mui/material/Fade";
+import { SxProps } from "@mui/material";
 
 const ViewDocument: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const params = useParams<{ id: string }>();
   const navigate = useNavigate();
   const [document, setDocument] = useState<EditorDocument | null>(null);
+  const slideTrigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
 
   useEffect(() => {
     const loadDocument = async (id: string) => {
@@ -48,14 +56,23 @@ const ViewDocument: React.FC = () => {
     return <SplashScreen title="Loading Document" />;
   }
 
+  const Fork = forwardRef((props: { sx?: SxProps }, ref: any) => <Fab ref={ref} variant="extended" size='medium' component={RouterLink} to="/new" state={{ data: document.data }}
+    sx={{ position: 'fixed', right: 24, bottom: 24, px: 2, displayPrint: 'none', zIndex: 1200, ...props.sx }}>
+    <EditIcon />Fork
+  </Fab>
+  );
+
   return document.id === params.id ? <>
     <Helmet><title>{document.name}</title></Helmet>
     <Editor document={document} readOnly />
-    <Fab variant="extended" size='medium' component={RouterLink} to="/new" state={{ data: document.data }}
-      sx={{ position: 'fixed', bottom: 24, right: 48, px: 2, displayPrint: 'none' }}>
-      <EditIcon />
-      Fork
-    </Fab>
+    <Fade in={!slideTrigger} timeout={{ exit: 0 }} unmountOnExit >
+      <Fork />
+    </Fade>
+    <Fade in={slideTrigger} unmountOnExit>
+      <Slide direction="left" in={slideTrigger} >
+        <Fork sx={{ right: 64 }} />
+      </Slide>
+    </Fade>
   </> : <SplashScreen />;
 }
 
