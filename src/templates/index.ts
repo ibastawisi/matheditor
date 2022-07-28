565cd121642827bc2bf66cabd3261cf64ab0eecc
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-07-28 08:56:59 +0200

    minor template rename

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,10 +1,10 @@
 import { EditorDocument } from '../slices/app';
-import ComplexSheet from './ComplexSheet.json';
+import ComplexAnalysis from './ComplexAnalysis.json';
 import FourierAnalysis from './FourierAnalysis.json';
 import RootLocus from './RootLocus.json';
 
 const templates = {
-  [ComplexSheet.id]: ComplexSheet as EditorDocument,
+  [ComplexAnalysis.id]: ComplexAnalysis as EditorDocument,
   [FourierAnalysis.id]: FourierAnalysis as EditorDocument,
   [RootLocus.id]: RootLocus as EditorDocument
 };
