d3a5d8ef4893758de479de166306a4a75ce470d9
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-08-05 17:46:08 +0200

    add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import './index.css';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './analytics';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from "react-router-dom";
@@ -19,12 +19,5 @@ root.render(
   </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToVercelAnalytics);
