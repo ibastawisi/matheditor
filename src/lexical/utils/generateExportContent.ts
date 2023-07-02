fb79ede8f63337475089c769c0cb301abbfcc22d
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-07-03 01:17:01 +0300

    replace sticky markdown with its contents

--- src/lexical/utils/generateExportContent.ts
+++ src/lexical/utils/generateExportContent.ts
@@ -10,8 +10,22 @@ export const generateMarkdown = (data: SerializedEditorState) => new Promise<str
   const editorState = editor.parseEditorState(data);
   editor.setEditorState(editorState);
   editorState.read(() => {
-    const markdown = $convertToMarkdownString(TRANSFORMERS);
-    resolve(markdown);
+    let markdown = $convertToMarkdownString(TRANSFORMERS);
+    const stickyRegex = /<sticky key="(.+?)" \/>/g;
+    const stickyMatches = markdown.match(stickyRegex);
+    if (!stickyMatches) return resolve(markdown);
+    const stickyKeys = stickyMatches.map((match) => match.replace(stickyRegex, '$1'));
+    const stickyNodes = stickyKeys.map((key) => editorState._nodeMap.get(key));
+    if (!stickyNodes.length) return resolve(markdown);
+    Promise.all(stickyNodes.map((node) => {
+      if (!$isStickyNode(node)) return Promise.resolve();
+      const data = node.getData();
+      if (!data) return Promise.resolve();
+      return generateMarkdown(data).then((result) => {
+        const stickyRegex = new RegExp(`<sticky key="${node.getKey()}" \/>`, 'g');
+        markdown = markdown.replace(stickyRegex, result + '\n\n');
+      })
+    })).then(() => resolve(markdown));
   })
 });
 
