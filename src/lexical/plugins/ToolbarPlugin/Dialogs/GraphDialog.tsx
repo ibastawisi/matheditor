310939aba0be8f50743686723d61e9b7fdf25e60
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-07-06 07:25:18 +0300

    fix graph dialog selection lost

--- src/lexical/plugins/ToolbarPlugin/Dialogs/GraphDialog.tsx
+++ src/lexical/plugins/ToolbarPlugin/Dialogs/GraphDialog.tsx
@@ -1,78 +1,85 @@
-import { LexicalEditor, UNDO_COMMAND } from 'lexical';
+import { LexicalEditor, $setSelection } from 'lexical';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import { INSERT_GRAPH_COMMAND } from '../../GraphPlugin';
 import { GraphNode, GraphType } from '../../../nodes/GraphNode';
-import { useEffect, useRef, useState } from 'react';
+import { memo, useEffect, useId, useRef, useState } from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { useSelector, useDispatch } from 'react-redux';
 import { actions, RootState } from '../../../../store';
+import { $getSelection } from 'lexical';
 
 export default function GraphDialog({ editor, node }: { editor: LexicalEditor, node: GraphNode | null; }) {
   const open = useSelector((state: RootState) => state.app.ui.dialogs.graph.open);
   const graphType = useSelector((state: RootState) => state.app.ui.dialogs.graph?.type) || GraphType["2D"];
   const dispatch = useDispatch();
-  const [parameters, setParameters] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const key = useId();
 
-  useEffect(() => {
-    if (!open) return;
-    const parameters = {
-      showToolBar: true,
-      borderColor: null,
-      showMenuBar: true,
-      allowStyleBar: true,
-      showAlgebraInput: true,
-      enableLabelDrags: false,
-      enableShiftDragZoom: true,
-      capturingThreshold: null,
-      showToolBarHelp: true,
-      errorDialogsActive: true,
-      showTutorialLink: true,
-      width: window.innerWidth,
-      height: window.innerHeight - 52.5,
-      appName: graphType === GraphType["2D"] ? 'suite' : '3d',
-      ggbBase64: node?.getValue() ?? "",
-      appletOnLoad() { setLoading(false); },
-    };
-
-    setParameters(parameters);
-  }, [open]);
+  const parameters = {
+    key,
+    showLogging: false,
+    showToolBar: true,
+    borderColor: null,
+    showMenuBar: true,
+    allowStyleBar: true,
+    showAlgebraInput: true,
+    enableLabelDrags: false,
+    enableShiftDragZoom: true,
+    capturingThreshold: null,
+    showToolBarHelp: true,
+    errorDialogsActive: true,
+    showTutorialLink: true,
+    width: window.innerWidth,
+    height: window.innerHeight - 52.5,
+    appName: graphType === GraphType["2D"] ? 'suite' : '3d',
+    ggbBase64: node?.getValue() ?? "",
+    appletOnLoad() { setLoading(false); },
+  };
 
   const insertGraph = (src: string, value: string) => {
     if (!node) editor.dispatchCommand(INSERT_GRAPH_COMMAND, { src, value, graphType },);
     else editor.update(() => node.update(src, value));
-  }
+  };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const app = (window as any).ggbApplet;
-    const src = "data:image/png;base64," + app.getPNGBase64(1, true, 72);
+    const src = await getBase64Src();
     const value = app.getBase64();
-    insertGraph(src, value);
+    editor.getEditorState().read(() => {
+      const selection = $getSelection()?.clone() ?? null;
+      editor.update(() => $setSelection(selection));
+      insertGraph(src, value);
+      handleClose();
+    })
+  };
+
+  const getBase64Src = () => new Promise<string>((resolve, reject) => {
+    const app = (window as any).ggbApplet;
     if (graphType === GraphType["2D"]) {
       app.exportSVG((html: string) => {
         const src = "data:image/svg+xml," + encodeURIComponent(html);
-        const value = app.getBase64() as string;
-        editor.dispatchCommand(UNDO_COMMAND, undefined);
-        insertGraph(src, value);
-        handleClose();
+        resolve(src);
       });
-    } else handleClose();
-  };
+    } else {
+      const src = "data:image/png;base64," + app.getPNGBase64(1, true, 72);
+      resolve(src);
+    }
+  });
+
 
   const handleClose = () => {
     dispatch(actions.app.setDialogs({ graph: { open: false, type: graphType } }));
     setLoading(true);
-    setParameters(null);
   }
 
   return <Dialog open={open} fullScreen={true} onClose={handleClose} disableEscapeKeyDown>
     <DialogContent sx={{ p: 0, overflow: "hidden" }}>
       {loading && <Box sx={{ display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center' }}><CircularProgress size={36} disableShrink /></Box>}
-      {parameters && <GeogebraApplet parameters={parameters} />}
+      <GeogebraApplet parameters={parameters} />
     </DialogContent>
     <DialogActions>
       <Button onClick={handleClose}>
@@ -85,7 +92,7 @@ export default function GraphDialog({ editor, node }: { editor: LexicalEditor, n
   </Dialog>;
 }
 
-const GeogebraApplet = ({ parameters }: { parameters: any }) => {
+const GeogebraApplet = memo(({ parameters }: { parameters: any }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -98,5 +105,4 @@ const GeogebraApplet = ({ parameters }: { parameters: any }) => {
   }, [parameters]);
 
   return <div ref={containerRef} />;
-
-}
+}, (prevProps, nextProps) => prevProps.parameters.key === nextProps.parameters.key);
