64e37e926784457efebaa3c82417fe59f6c6a517
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-07-09 21:44:48 +0300

    add link to last commit in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,19 @@ import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography";
 import packageJson from '../../package.json';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from "@mui/material/Link";
 
 const Footer: React.FC = (props: any) => {
   const version = packageJson.version;
   const commitHash: string | undefined = import.meta.env.VITE_VERCEL_GIT_COMMIT_SHA;
   return (
-    <Box component="footer" sx={{ displayPrint: "none", mt: "auto", p: 0.5 }}>
-      <Typography variant="body2" color="text.secondary" sx={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-        v{version}{commitHash && <><GitHubIcon fontSize="inherit" sx={{ mx: 0.5 }} /> {commitHash?.substring(0, 7)}</>}
+    <Box component="footer" sx={{ displayPrint: "none", mt: "auto", p: 0.5, textAlign: "end" }}>
+      <Typography variant="button" color="text.secondary">
+        v{version}
+        {commitHash && <Link href={`https://github.com/ibastawisi/matheditor/commit/${commitHash?.substring(0, 7)}`} target="_blank"
+          sx={{ textDecoration: "none" }}>
+          <GitHubIcon fontSize="inherit" sx={{ ml: 0.5, verticalAlign: "middle" }} /> {commitHash?.substring(0, 7)}
+        </Link>}
       </Typography>
     </Box>
   )
