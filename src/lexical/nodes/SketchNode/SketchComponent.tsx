13b6654138085e31ef221675d3906d2705a4864c
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-07-10 07:58:57 +0300

    refactor math and sketch components

---
+++ src/lexical/nodes/SketchNode/SketchComponent.tsx
@@ -0,0 +1,54 @@
+import { NodeKey } from 'lexical';
+import { useEffect, useState } from 'react';
+import { NonDeleted, ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
+import { ImageComponent } from '../ImageNode/ImageComponent';
+import Virgil from "@excalidraw/excalidraw/dist/excalidraw-assets/Virgil.woff2";
+import Cascadia from "@excalidraw/excalidraw/dist/excalidraw-assets/Cascadia.woff2";
+
+export const encodeFonts = Promise.all([
+  fetch(Virgil).then(res => res.arrayBuffer()).then(buffer => {
+    new FontFace("Virgil", buffer).load().then(font => document.fonts.add(font));
+    return arrayBufferToBase64Font(buffer)
+  }),
+  fetch(Cascadia).then(res => res.arrayBuffer()).then(async buffer => {
+    new FontFace("Cascadia", buffer).load().then(font => document.fonts.add(font));
+    return arrayBufferToBase64Font(buffer)
+  })
+]);
+
+const arrayBufferToBase64Font = (buffer: ArrayBuffer) => {
+  const bytes = new Uint8Array(buffer);
+  const binary = bytes.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
+  return `data:font/woff2;base64,${btoa(binary)}`;
+}
+
+export function SketchComponent({
+  nodeKey, width, height, src, value, resizable,
+}: {
+  width: 'inherit' | number;
+  height: 'inherit' | number;
+  src: string;
+  nodeKey: NodeKey;
+  resizable: boolean;
+  value?: NonDeleted<ExcalidrawElement>[];
+}): JSX.Element {
+
+  const [source, setSource] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function embedFonts() {
+      const [virgil, cascadia] = await encodeFonts;
+      const fonts = `@font-face { font-family: 'Virgil'; src: url('${virgil}') format('woff2');} @font-face { font-family: 'Cascadia'; src: url('${cascadia}') format('woff2'); }`;
+      const encoded = src.substring(src.indexOf(',') + 1);
+      const decoded = decodeURIComponent(encoded);
+      const serialized = decoded.replace(/<style.*?>[\s\S]*<\/style>/, `<style class="style-fonts">${fonts}</style>`);
+
+      setSource(`data:image/svg+xml,${encodeURIComponent(serialized)}`);
+    };
+    embedFonts();
+  }, [src]);
+
+  return (
+    <ImageComponent nodeKey={nodeKey} width={width} height={height} src={source || src} altText="" resizable={resizable} />
+  );
+}
--- src/lexical/nodes/SketchNode/index.tsx
+++ src/lexical/nodes/SketchNode/index.tsx
@@ -7,33 +7,9 @@
  */
 
 import { $createNodeSelection, $setSelection, DOMConversionMap, DOMConversionOutput, DOMExportOutput, EditorConfig, LexicalNode, NodeKey, SerializedLexicalNode, Spread, } from 'lexical';
-
 import { DecoratorNode, } from 'lexical';
-
-import { useEffect, useState } from 'react';
-
 import { NonDeleted, ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
-import { ImageComponent } from '../ImageNode/ImageComponent';
-
-import Virgil from "@excalidraw/excalidraw/dist/excalidraw-assets/Virgil.woff2";
-import Cascadia from "@excalidraw/excalidraw/dist/excalidraw-assets/Cascadia.woff2";
-
-const encodeFonts = Promise.all([
-  fetch(Virgil).then(res => res.arrayBuffer()).then(buffer => {
-    new FontFace("Virgil", buffer).load().then(font => document.fonts.add(font));
-    return arrayBufferToBase64Font(buffer)
-  }),
-  fetch(Cascadia).then(res => res.arrayBuffer()).then(async buffer => {
-    new FontFace("Cascadia", buffer).load().then(font => document.fonts.add(font));
-    return arrayBufferToBase64Font(buffer)
-  })
-]);
-
-const arrayBufferToBase64Font = (buffer: ArrayBuffer) => {
-  const bytes = new Uint8Array(buffer);
-  const binary = bytes.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
-  return `data:font/woff2;base64,${btoa(binary)}`;
-}
+import { SketchComponent } from './SketchComponent';
 
 export interface SketchPayload {
   key?: NodeKey;
@@ -55,42 +31,6 @@ function convertSketchElement(domNode: Node): null | DOMConversionOutput {
     return { node };
   }
   return null;
-}
-
-function SketchComponent({
-  nodeKey,
-  width,
-  height,
-  src,
-  value,
-  resizable,
-}: {
-  width: 'inherit' | number;
-  height: 'inherit' | number;
-  src: string;
-  nodeKey: NodeKey;
-  resizable: boolean;
-  value?: NonDeleted<ExcalidrawElement>[];
-}): JSX.Element {
-
-  const [source, setSource] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function embedFonts() {
-      const [virgil, cascadia] = await encodeFonts;
-      const fonts = `@font-face { font-family: 'Virgil'; src: url('${virgil}') format('woff2');} @font-face { font-family: 'Cascadia'; src: url('${cascadia}') format('woff2'); }`;
-      const encoded = src.substring(src.indexOf(',') + 1);
-      const decoded = decodeURIComponent(encoded);
-      const serialized = decoded.replace(/<style.*?>[\s\S]*<\/style>/, `<style class="style-fonts">${fonts}</style>`);
-
-      setSource(`data:image/svg+xml,${encodeURIComponent(serialized)}`);
-    };
-    embedFonts();
-  }, [src]);
-
-  return (
-    <ImageComponent nodeKey={nodeKey} width={width} height={height} src={source || src} altText="" resizable={resizable} />
-  );
 }
 
 export type SerializedSketchNode = Spread<
