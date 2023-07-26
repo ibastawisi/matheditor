feefc1a1bf34d0de51cb2503d3f4a1bf8b7c1187
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-07-26 03:46:30 +0300

    exclude manifest from precaching

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@
 const withPWA = require("@imbios/next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === 'development',
+  buildExcludes: ["app-build-manifest.json"],
+  skipWaiting: false,
 });
 
 
