50b8ef10dc5af338398a713f682e96c1bac0dc85
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-08-03 18:44:20 +0300

    enable context menu on mobile

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -4,18 +4,17 @@ import { LexicalComposer, InitialConfigType } from "@lexical/react/LexicalCompos
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import TreeViewPlugin from "./plugins/TreeViewPlugin";
 import ToolbarPlugin from "./plugins/ToolbarPlugin";
-import { IS_MOBILE } from './shared/environment';
 import { editorConfig } from "./config";
 import { EditorPlugins } from "./plugins";
 import "./styles.css";
 
 export const Editor: React.FC<{ initialConfig: Partial<InitialConfigType>; onChange: (editorState: EditorState) => void; }> = ({ initialConfig, onChange }) => {
-  const disableContextMenu = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => { if (IS_MOBILE) e.preventDefault(); };
+  // const disableContextMenu = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => { if (IS_MOBILE) e.preventDefault(); };
   return (
     <LexicalComposer initialConfig={{ ...editorConfig, ...initialConfig }}>
       <>
         <ToolbarPlugin />
-        <EditorPlugins contentEditable={<ContentEditable className="editor-input" onContextMenu={disableContextMenu} />} onChange={onChange} />
+        <EditorPlugins contentEditable={<ContentEditable className="editor-input" />} onChange={onChange} />
         {process.env.NODE_ENV === "development" && <TreeViewPlugin />}
       </>
     </LexicalComposer>
