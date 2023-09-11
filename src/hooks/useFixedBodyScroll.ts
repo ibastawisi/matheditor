4ffec9d4f242898e343e97dbd29673df4acd5ced
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-09-11 11:53:17 +0300

    reset scrollTop on dialog close

--- src/hooks/useFixedBodyScroll.ts
+++ src/hooks/useFixedBodyScroll.ts
@@ -3,20 +3,15 @@ import { useEffect, useRef } from "react";
 export default function useFixedBodyScroll(isFixed: boolean) {
   const scrollTop = useRef(0);
   useEffect(() => {
-    if (!isFixed && !scrollTop.current) return;
-    if (isFixed) {
-      scrollTop.current = Math.round(document.documentElement.scrollTop);
-      document.body.style.top = `-${scrollTop.current}px`;
-      document.body.style.position = "fixed";
-    } else {
-      document.body.style.position = "";
-      document.body.style.top = "";
-      document.documentElement.scrollTop = scrollTop.current;
-    }
+    if (!isFixed) return;
+    scrollTop.current = Math.round(document.documentElement.scrollTop);
+    document.body.style.top = `-${scrollTop.current}px`;
+    document.body.style.position = "fixed";
     return () => {
       document.body.style.position = "";
       document.body.style.top = "";
       document.documentElement.scrollTop = scrollTop.current;
+      scrollTop.current = 0;
     }
   }, [isFixed]);
 }
