a01c7570ec1fce55fa06f584d972598864992db1
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-02-08 09:47:05 +0200

    show error code in production

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,9 @@
 import SplashScreen from '@/components/SplashScreen'
 
 export default function Error({ error }: { error: Error, reset: () => void }) {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const message = isProduction ? `Error Code: ${(error as any).digest}` : error.message;
   return (
-    <SplashScreen title="Something went wrong" subtitle={error.message} />
+    <SplashScreen title="Something went wrong" subtitle={message} />
   )
 }
