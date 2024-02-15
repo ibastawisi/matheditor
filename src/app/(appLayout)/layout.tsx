bcf2930b55056ef331294746ca91e2e7b5bf9f97
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-02-15 15:29:38 +0200

    add adsense script

--- src/app/(appLayout)/layout.tsx
+++ src/app/(appLayout)/layout.tsx
@@ -1,9 +1,16 @@
 import AppLayout from "@/components/Layout/AppLayout";
+import Script from "next/script";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const PUBLISHER_ID = process.env.PUBLISHER_ID;
   return (
-    <AppLayout>
-      {children}
-    </AppLayout>
+    <>
+      {PUBLISHER_ID && <Script async crossOrigin="anonymous" strategy="lazyOnload"
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${PUBLISHER_ID}`}
+      />}
+      <AppLayout>
+        {children}
+      </AppLayout>
+    </>
   )
 }
