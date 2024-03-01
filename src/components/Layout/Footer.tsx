d9a86b9f8d601e5c76d45b7067837046c867ee9c
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-03-01 19:56:05 +0200

    add consent management button

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,8 @@
-import { Cached } from '@mui/icons-material';
+import { AdminPanelSettings, Cached } from '@mui/icons-material';
 import packageJson from '../../../package.json';
 import { Box, Typography, Link, IconButton } from '@mui/material';
 import RouterLink from "next/link";
+import Script from 'next/script';
 
 const Footer: React.FC = () => {
   const version = packageJson.version;
@@ -12,13 +13,23 @@ const Footer: React.FC = () => {
       <Typography variant="button" component={Link} href={href} target="_blank" sx={{ textDecoration: "none", display: "flex", alignItems: "center" }}>
         v{version} {commitHash?.substring(0, 7)}
       </Typography>
-      <IconButton size="small" sx={{ width: 24, height: 24 }} aria-label="Check for updates">
-        <script dangerouslySetInnerHTML={{__html: `document.currentScript.parentElement.onclick = () => { if ('serviceWorker' in navigator && navigator.onLine) { navigator.serviceWorker.getRegistrations().then(registrations => { registrations.forEach(registration => registration.unregister()) }).then(() => window.location.reload());}}`}} />
+      <IconButton size="small" sx={{ width: 24, height: 24 }} aria-label="Check for updates" LinkComponent="a"
+        href="javascript: if ('serviceWorker' in navigator && navigator.onLine) { navigator.serviceWorker.getRegistrations().then(registrations => { registrations.forEach(registration => registration.unregister()) }).then(() => window.location.reload());}">
         <Cached />
       </IconButton>
       <Typography variant="button">
         <Link component={RouterLink} prefetch={false} href="/privacy" sx={{ textDecoration: "none" }}>Privacy Policy</Link>
       </Typography>
+      <IconButton size="small" sx={{ width: 24, height: 24 }} aria-label="Manage consent" LinkComponent="a"
+        href="javascript:googlefc.callbackQueue.push(googlefc.showRevocationMessage)">
+        <AdminPanelSettings />
+      </IconButton>
+      <Script
+        id="_next-ga-init"
+        dangerouslySetInnerHTML={{
+          __html: `new MutationObserver(() => { document.querySelector(".google-revocation-link-placeholder")?.remove(); }).observe(document.body, { childList: true, });`
+        }}
+      />
     </Box>
   )
 }
