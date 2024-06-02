c31d7dfa6cb317f2edce3d05dffdb53a3361571e
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-06-02 21:32:38 +0300

    deploy script git hard reset

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ CURRENT_LIVE_DIR=false
 
 DEPLOYING_TO_NAME=false
 DEPLOYING_TO_DIR=false
-DEPLOYING_TO_PORT=false
 
 # this checks if the green and blue apps are running
 GREEN_ONLINE=$(pm2 jlist | jq -r '.[] | select(.name == "matheditor-green") | .name, .pm2_env.status' | tr -d '\n\r')
@@ -26,7 +25,6 @@ if [ "$BLUE_ONLINE" == "matheditor-blueonline" ]; then
     CURRENT_LIVE_DIR=$BLUE_DIR
     
     DEPLOYING_TO_NAME="matheditor-green"
-    DEPLOYING_TO_PORT=3001
     DEPLOYING_TO_DIR=$GREEN_DIR
 fi
 
@@ -37,7 +35,6 @@ if [ "$GREEN_ONLINE" == "matheditor-greenonline" ]; then
     CURRENT_LIVE_DIR=$GREEN_DIR
     
     DEPLOYING_TO_NAME="matheditor-blue"
-    DEPLOYING_TO_PORT=3000
     DEPLOYING_TO_DIR=$BLUE_DIR
 fi
 
@@ -53,8 +50,8 @@ cd $DEPLOYING_TO_DIR || { echo 'Could not access deployment directory.' ; exit 1
 
 # Pull the latest changes
 # clear any changes to avoid conflicts (usually permission based)
-git reset --hard || { echo 'Git reset command failed' ; exit 1; }
-git pull -f origin master || { echo 'Git pull command failed' ; exit 1; }
+git fetch || { echo 'Git fetch command failed' ; exit 1; }
+git reset --hard origin/master || { echo 'Git reset command failed' ; exit 1; }
 
 # install the dependencies
 npm ci || { echo 'npm clean install failed' ; exit 1; }
@@ -64,7 +61,7 @@ npx prisma migrate deploy || { echo 'Prisma migrate deploy failed' ; exit 1; }
 npm run build || { echo 'Build failed' ; exit 1; }
 
 # start the pm2 process
-pm2 start npm --name $DEPLOYING_TO_NAME -- start -- --port=$DEPLOYING_TO_PORT || { echo 'pm2 start failed' ; exit 1; }
+pm2 start npm --name $DEPLOYING_TO_NAME -- start || { echo 'pm2 start failed' ; exit 1; }
 
 # add a delay to allow the server to start
 sleep 5
