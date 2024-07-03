ee109bec05efe529fe6b04137a2dd8afcd0b904e
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-07-04 01:17:28 +0300

    add text format shortcuts

--- src/editor/plugins/ToolbarPlugin/Tools/TextFormatToggles.tsx
+++ src/editor/plugins/ToolbarPlugin/Tools/TextFormatToggles.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as React from 'react';
-import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND, LexicalEditor, COMMAND_PRIORITY_CRITICAL, SELECTION_CHANGE_COMMAND, TextFormatType, } from "lexical";
+import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND, LexicalEditor, COMMAND_PRIORITY_CRITICAL, SELECTION_CHANGE_COMMAND, TextFormatType, KEY_MODIFIER_COMMAND, COMMAND_PRIORITY_NORMAL, } from "lexical";
 import { $patchStyleText, } from '@lexical/selection';
 import { mergeRegister, IS_APPLE } from '@lexical/utils';
 import { $isLinkNode } from '@lexical/link';
@@ -15,7 +15,7 @@ import { FormatBold, FormatItalic, FormatUnderlined, Code, FormatStrikethrough, 
 import { getSelectedNode } from '@/editor/utils/getSelectedNode';
 import { SET_DIALOGS_COMMAND } from '../Dialogs/commands';
 
-const Highlight = () => <SvgIcon viewBox='0 -960 960 960' fontSize='small'>
+const Highlight = () => <SvgIcon viewBox='0 -960 960 960'>
   <path xmlns="http://www.w3.org/2000/svg" d="M80 0v-160h800V0H80Zm504-480L480-584 320-424l103 104 161-160Zm-47-160 103 103 160-159-104-104-159 160Zm-84-29 216 216-189 190q-24 24-56.5 24T367-263l-27 23H140l126-125q-24-24-25-57.5t23-57.5l189-189Zm0 0 187-187q24-24 56.5-24t56.5 24l104 103q24 24 24 56.5T857-640L669-453 453-669Z" />
 </SvgIcon>;
 
@@ -56,7 +56,7 @@ export default function TextFormatToggles({ editor, sx }: { editor: LexicalEdito
   useEffect(() => {
     return editor.registerCommand(
       SELECTION_CHANGE_COMMAND,
-      (_payload, newEditor) => {
+      (_payload) => {
         updateToolbar();
         return false;
       },
@@ -93,7 +93,7 @@ export default function TextFormatToggles({ editor, sx }: { editor: LexicalEdito
     applyStyleText({ [styleKey]: value });
   }, [applyStyleText]);
 
-  const handleFormat = (event: React.MouseEvent<HTMLElement>, newFormats: string[]) => {
+  const handleFormat = (event: React.MouseEvent<HTMLElement>) => {
     const button = event.currentTarget as HTMLButtonElement;
     if (!button) return;
     editor.dispatchCommand(FORMAT_TEXT_COMMAND, button.value as TextFormatType);
@@ -111,6 +111,36 @@ export default function TextFormatToggles({ editor, sx }: { editor: LexicalEdito
     }, [] as string[],
   );
 
+  useEffect(() => {
+    return editor.registerCommand(
+      KEY_MODIFIER_COMMAND,
+      (payload) => {
+        const event: KeyboardEvent = payload;
+        const { code, ctrlKey, metaKey, shiftKey } = event;
+        if (code === 'KeyK' && (ctrlKey || metaKey)) {
+          event.preventDefault();
+          return openLinkDialog();
+        }
+        if (code === 'KeyH' && (ctrlKey || metaKey) && shiftKey) {
+          event.preventDefault();
+          return editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'highlight');
+        }
+        if (code === 'KeyE' && (ctrlKey || metaKey)) {
+          event.preventDefault();
+          return editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code');
+        }
+        if (code === 'KeyS' && (ctrlKey || metaKey) && shiftKey) {
+          event.preventDefault();
+          return editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough');
+        }
+        return false;
+      },
+      COMMAND_PRIORITY_NORMAL,
+    );
+  }, [editor]);
+
+
+
   const openLinkDialog = () => editor.dispatchCommand(SET_DIALOGS_COMMAND, ({ link: { open: true } }));
 
   return (<ToggleButtonGroup size="small" sx={{ ...sx }} value={formats} onChange={handleFormat} aria-label="text formatting">
@@ -123,22 +153,22 @@ export default function TextFormatToggles({ editor, sx }: { editor: LexicalEdito
     <ToggleButton value="underline" title={IS_APPLE ? 'Underline (⌘U)' : 'Underline (Ctrl+U)'} aria-label={`Format text to underlined. Shortcut: ${IS_APPLE ? '⌘U' : 'Ctrl+U'}`}>
       <FormatUnderlined />
     </ToggleButton>
-    <ToggleButton value="highlight" title='Highlight selected text' aria-label='Highlight selected text'>
+    <ToggleButton value="highlight" title={IS_APPLE ? 'Highlight (⌘+⇧+H)' : 'Highlight (Ctrl+Shift+H)'} aria-label={`Format text as highlight. Shortcut: ${IS_APPLE ? '⌘+⇧+H' : 'Ctrl+Shift+H'}`}>
       <Highlight />
     </ToggleButton>
-    <ToggleButton value="code" title='Format text to inline code' aria-label='Format text to inline code'>
+    <ToggleButton value="code" title={IS_APPLE ? 'Inline code (⌘E)' : 'Inline code (Ctrl+E)'} aria-label={`Format text as Inline code. Shortcut: ${IS_APPLE ? '⌘E' : 'Ctrl+E'}`}>
       <Code />
     </ToggleButton>
-    <ToggleButton value="strikethrough" title='Format text with strikethrough' aria-label='Format text with strikethrough'>
+    <ToggleButton value="strikethrough" title={IS_APPLE ? 'Strikethrough (⌘+⇧+S)' : 'Strikethrough (Ctrl+Shift+S)'} aria-label={`Format text as strikethrough. Shortcut: ${IS_APPLE ? '⌘+⇧+S' : 'Ctrl+Shift+S'}`}>
       <FormatStrikethrough />
     </ToggleButton>
-    <ToggleButton value="subscript" title='Format text with subscript' aria-label='Format text with subscript'>
+    <ToggleButton value="subscript" title='Subscript' aria-label='Format text with subscript'>
       <Subscript />
     </ToggleButton>
-    <ToggleButton value="superscript" title='Format text with superscript' aria-label='Format text with superscript'>
+    <ToggleButton value="superscript" title='Superscript' aria-label='Format text with superscript'>
       <Superscript />
     </ToggleButton>
-    <ToggleButton value="link" title='Insert link' aria-label='Insert link' onClick={openLinkDialog}>
+    <ToggleButton value="link" title={IS_APPLE ? 'Insert Link (⌘K)' : 'Insert Link (Ctrl+K)'} aria-label={`Insert a link. Shortcut: ${IS_APPLE ? '⌘K' : 'Ctrl+K'}`} onClick={openLinkDialog}>
       <Link />
     </ToggleButton>
     <ColorPicker onColorChange={onColorChange} />
