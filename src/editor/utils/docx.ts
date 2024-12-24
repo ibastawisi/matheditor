3e57faa6f40a84e5181acc50b197b89d6e82b777
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2024-12-24 13:58:08 +0200

    fix nested list items

--- src/editor/utils/docx.ts
+++ src/editor/utils/docx.ts
@@ -1,50 +1,37 @@
-import { convertInchesToTwip, Document, FileChild, ImageRun, IParagraphOptions, Math, MathRun, Packer, Paragraph, ParagraphChild, TextRun } from "docx";
-import { $getRoot, LexicalNode, $isElementNode, $isTextNode, $isParagraphNode } from "lexical";
+import { AlignmentType, convertInchesToTwip, Document, FileChild, ILevelsOptions, ImageRun, IParagraphOptions, LevelFormat, Math, MathRun, Packer, Paragraph, ParagraphChild, TextRun } from "docx";
+import { $getRoot, LexicalNode, $isElementNode, $isTextNode, $isParagraphNode, $isLineBreakNode } from "lexical";
 import { $getNodeStyleValueForProperty } from "../nodes/utils";
-import { $isHeadingNode, $isImageNode, $isMathNode } from "..";
+import { $isHeadingNode, $isHorizontalRuleNode, $isImageNode, $isListItemNode, $isListNode, $isMathNode, ListNode } from "..";
 
 export function $getDocxFileChildren() {
   const root = $getRoot();
-  const topLevelChildren = root.getChildren();
-  const children: FileChild[] = [];
-  for (let i = 0; i < topLevelChildren.length; i++) {
-    const topLevelNode = topLevelChildren[i];
-    const child = $exportNodeToDocx(topLevelNode) as FileChild;
-    if (child) children.push(child);
-  }
-  return children;
+  const elements = $exportNodeToDocx(root);
+  return elements as FileChild[];
 }
 
-function $exportNodeToDocx(node: LexicalNode,) {
+function $exportNodeToDocx(node: LexicalNode): FileChild | ParagraphChild | ParagraphChild[] | null {
   const element = $mapNodeToDocx(node);
-  if (!element) return false;
-  if (element instanceof Paragraph) {
-    const childNodes = $isElementNode(node) ? node.getChildren() : [];
-    for (let i = 0; i < childNodes.length; i++) {
-      const childNode = childNodes[i];
-      const children = $mapNodeToDocx(childNode);
-      if (!children) continue;
-      if (Array.isArray(children)) children.forEach((child) => element.addChildElement(child as any));
-      else element.addChildElement(children as any);
-    }
-  }
+  const childNodes = $isElementNode(node) ? node.getChildren() : [];
+  if (childNodes.length === 0) return element;
+  const children = childNodes.map($exportNodeToDocx).filter(Boolean).flat() as FileChild[];
+  if (!element) return children
+  if (element instanceof FileChild) children.forEach((child) => element.addChildElement(child));
   return element;
 }
 
-// Define the function to map a Lexical node to a DOCX element
 function $mapNodeToDocx(node: LexicalNode): FileChild | ParagraphChild | ParagraphChild[] | null {
-  // debugger;
   if ($isTextNode(node)) {
     const textContent = node.getTextContent();
     const parent = node.getParent();
     const isHeadingText = $isHeadingNode(parent);
+    const isCheckedText = $isListItemNode(parent) && parent.getChecked();
     const fontsizeInPx = parseInt($getNodeStyleValueForProperty(node, 'font-size'));
     const backgroundColor = $getNodeStyleValueForProperty(node, 'background-color');
     const textRun = new TextRun({
       text: textContent,
       bold: node.hasFormat('bold') || isHeadingText,
       italics: node.hasFormat('italic'),
-      strike: node.hasFormat('strikethrough'),
+      strike: node.hasFormat('strikethrough') || isCheckedText,
       underline: node.hasFormat('underline') ? { type: "single" } : undefined,
       color: $getNodeStyleValueForProperty(node, 'color'),
       highlight: node.hasFormat('highlight') ? 'yellow' : undefined,
@@ -66,7 +53,6 @@ function $mapNodeToDocx(node: LexicalNode): FileChild | ParagraphChild | Paragra
     return new Paragraph({
       alignment,
       indent: { left: convertInchesToTwip(indent / 2) },
-      spacing: { after: 6 }
     });
   }
   if ($isHeadingNode(node)) {
@@ -113,80 +99,127 @@ function $mapNodeToDocx(node: LexicalNode): FileChild | ParagraphChild | Paragra
     const captionChildren = caption.getEditorState().read($getDocxFileChildren);
     return [imageRun, ...captionChildren];
   }
-  // Return null if the node type is unsupported
+
+  if ($isListItemNode(node)) {
+    const firstChild = node.getFirstChild();
+    if ($isListNode(firstChild)) return null;
+    const alignment = node.getFormatType().replace('justify', 'both') as IParagraphOptions['alignment'];
+    const indent = node.getIndent();
+    const ListNode = node.getParent() as ListNode;
+    const listType = ListNode.getListType();
+    const checked = node.getChecked();
+    return new Paragraph({
+      alignment,
+      numbering: {
+        reference: `${listType}-list${listType === 'check' && checked ? '-checked' : ''}`,
+        level: indent,
+      },
+    });
+  }
+
+  if ($isLineBreakNode(node)) {
+    return new TextRun({ text: '', break: 1 });
+  }
+
+  if ($isHorizontalRuleNode(node)) {
+    return new Paragraph({
+      spacing: {
+        after: 8 * 15,
+        line: 2 * 15,
+      },
+      border: {
+        top: {
+          color: 'auto',
+          space: 1,
+          size: 6,
+          style: 'single',
+        },
+      },
+    });
+  }
   return null;
 }
 
-// Generate DOCX from nodes
-export async function generateDocxBlob(children: FileChild[]): Promise<Blob> {
+type Index = 1 | 2 | 3 | 4 | 5 | 6
+type HeadingMap = Record<Index, number>
+
+const headingSize: HeadingMap = { 1: 32 * 4 / 3, 2: 24 * 4 / 3, 3: 18.72 * 4 / 3, 4: 16 * 4 / 3, 5: 13.28 * 4 / 3, 6: 10.72 * 4 / 3 }
+const headingSpacing: HeadingMap = { 1: 21.44 * 15, 2: 19.92 * 15, 3: 18.72 * 15, 4: 21.28 * 15, 5: 22.178 * 15, 6: 24.978 * 15 }
+
+const heading = (level: Index) => ({
+  run: {
+    bold: true,
+    size: headingSize[level],
+  },
+  paragraph: {
+    spacing: {
+      after: headingSpacing[level],
+      before: headingSpacing[level],
+    },
+  },
+})
+
+function basicIndentStyle(indent: number): Pick<ILevelsOptions, 'style' | 'alignment'> {
+  return {
+    alignment: AlignmentType.START,
+    style: {
+      paragraph: {
+        indent: { left: convertInchesToTwip(indent), hanging: convertInchesToTwip(0.18) },
+      },
+    },
+  };
+}
+
+const numbered = Array(3)
+  .fill([LevelFormat.DECIMAL,LevelFormat.UPPER_LETTER, LevelFormat.LOWER_LETTER])
+  .flat()
+  .map((format, level) => ({
+    level,
+    format,
+    text: `%${level + 1}.`,
+    ...basicIndentStyle((level + 1) / 4),
+  }));
+
+const bullets = Array(3)
+  .fill(['●', '○', '■'])
+  .flat()
+  .map((text, level) => ({
+    level,
+    format: LevelFormat.BULLET,
+    text,
+    ...basicIndentStyle((level + 1) / 4),
+  }));
+
+const unchecked = Array(3)
+  .fill(['🗆', '🗆', '🗆'])
+  .flat()
+  .map((text, level) => ({
+    level,
+    text,
+    ...basicIndentStyle((level + 1) / 4),
+  }));
+
+const checked = Array(3)
+  .fill(['🗹', '🗹', '🗹'])
+  .flat()
+  .map((text, level) => ({
+    level,
+    text,
+    ...basicIndentStyle((level + 1) / 2),
+  }));
+
+export async function $generateDocxBlobFromEditor(): Promise<Blob> {
+  const root = $getRoot();
+  const children = $exportNodeToDocx(root) as FileChild[];
   const doc = new Document({
     styles: {
       default: {
-        heading1: {
-          run: {
-            size: 28,
-          },
-          paragraph: {
-            spacing: {
-              after: 120,
-            },
-          },
-        },
-        heading2: {
-          run: {
-            size: 26,
-          },
-          paragraph: {
-            spacing: {
-              before: 240,
-              after: 120,
-            },
-          },
-        },
-        heading3: {
-          run: {
-            size: 24,
-          },
-          paragraph: {
-            spacing: {
-              before: 240,
-              after: 120,
-            },
-          },
-        },
-        heading4: {
-          run: {
-            size: 22,
-          },
-          paragraph: {
-            spacing: {
-              before: 240,
-              after: 120,
-            },
-          },
-        },
-        heading5: {
-          run: {
-            size: 20,
-          },
-          paragraph: {
-            spacing: {
-              before: 240,
-              after: 120,
-            },
-          },
-        },
-        heading6: {
-          run: {
-            size: 18,
-          },
-          paragraph: {
-            spacing: {
-              before: 240,
-              after: 120,
-            },
-          },
-        },
+        heading1: heading(1),
+        heading2: heading(2),
+        heading3: heading(3),
+        heading4: heading(4),
+        heading5: heading(5),
+        heading6: heading(6),
         document: {
           run: {
             size: "12pt",
@@ -194,7 +227,8 @@ export async function generateDocxBlob(children: FileChild[]): Promise<Blob> {
           },
           paragraph: {
             spacing: {
-              line: 276,
+              line: 20 * 15,
+              after: 8 * 15,
             },
           },
         },
@@ -206,6 +240,26 @@ export async function generateDocxBlob(children: FileChild[]): Promise<Blob> {
         children: children,
       },
     ],
+    numbering: {
+      config: [
+        {
+          reference: 'number-list',
+          levels: numbered,
+        },
+        {
+          reference: 'bullet-list',
+          levels: bullets,
+        },
+        {
+          reference: 'check-list',
+          levels: unchecked,
+        },
+        {
+          reference: 'check-list-checked',
+          levels: checked,
+        },
+      ]
+    }
   });
 
   const buffer = await Packer.toBuffer(doc);
--- src/editor/utils/generateDocx.ts
+++ src/editor/utils/generateDocx.ts
@@ -1,18 +1,15 @@
 import type { SerializedEditorState } from "lexical";
 import { createHeadlessEditor } from "@lexical/headless";
 import { editorConfig } from "../config";
-import { $getDocxFileChildren, generateDocxBlob } from "./docx";
+import { $generateDocxBlobFromEditor } from "./docx";
 
 const editor = createHeadlessEditor(editorConfig);
 
 export const generateDocx = (data: SerializedEditorState) => new Promise<Blob>((resolve, reject) => {
   try {
-    // const children = toDocxSection(data as LexicalJson)
-    // toDocx([{ children }], numbering).then(resolve)
     const editorState = editor.parseEditorState(data);
     editor.setEditorState(editorState);
-    const children = editorState.read($getDocxFileChildren);
-    const blob = generateDocxBlob(children);
+    const blob = editorState.read($generateDocxBlobFromEditor);
     resolve(blob);
   } catch (error) {
     reject(error);
