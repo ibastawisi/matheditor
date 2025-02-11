d36babd302cd1b66eac2952cd4292f6aa849e26a
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2025-02-11 22:46:38 +0200

    catch math docx exceptions

--- src/editor/utils/docx/math.ts
+++ src/editor/utils/docx/math.ts
@@ -4,14 +4,17 @@ import { convertLatexToMathMl } from "mathlive";
 import { mml2omml } from "./mathml2omml";
 
 export function $convertMathNode(node: MathNode) {
-  const value = node.getValue();
-  const mathml = convertLatexToMathMl(value);
-  const ommlString: any = mml2omml(`<math xmlns="http://www.w3.org/1998/Math/MathML">${mathml}</math>`);
-
-  const xmlComponent: any = ImportedXmlComponent.fromXmlString(ommlString);
-  const mathRun = xmlComponent.root[0];
-  const id = node.getId();
-  if (!id) return mathRun;
-  const linkId = bookmarkUniqueNumericIdGen()();
-  return [new BookmarkStart(id, linkId), mathRun, new BookmarkEnd(linkId)];
+  try {
+    const value = node.getValue();
+    const mathml = convertLatexToMathMl(value);
+    const ommlString: any = mml2omml(`<math xmlns="http://www.w3.org/1998/Math/MathML">${mathml}</math>`);
+    const xmlComponent: any = ImportedXmlComponent.fromXmlString(ommlString);
+    const mathRun = xmlComponent.root[0];
+    const id = node.getId();
+    if (!id) return mathRun;
+    const linkId = bookmarkUniqueNumericIdGen()();
+    return [new BookmarkStart(id, linkId), mathRun, new BookmarkEnd(linkId)];
+  } catch (e) {
+    return null;
+  }
 }
